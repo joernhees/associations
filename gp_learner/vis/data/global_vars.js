499088c52c7178912c266d2ada4bb1172e43ea9f
Joern Hees <dev@joernhees.de>
2016-05-02 21:55:10 +0200

    included graph pattern learner paper + visualisation

---
+++ gp_learner/vis/data/global_vars.js
@@ -0,0 +1,4 @@
+var RUNS_GENS_DICT = JSON.parse('{"1": 8, "2": 17, "3": 20, "4": 13, "5": 20, "6": 20, "7": 9, "8": 16, "9": 20, "10": 5, "11": 5, "12": 16, "13": 12, "14": 20, "15": 5, "16": 20, "17": 20, "18": 15, "19": 13, "20": 5, "21": 15, "22": 11, "23": 13}');
+//var START_FILENAME = 'top_graph_patterns_run_01_gen_08.json.gz';
+var START_FILENAME = 'results.json.gz';
+var SCRIPT_ROOT = 'data';

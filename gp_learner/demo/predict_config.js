b809741d9502696f4f9ca65ac4d4f7ea7c8fcc2d
Joern Hees <dev@joernhees.de>
2017-07-11 16:55:42 +0200

    WIP: feedback buttons

--- gp_learner/demo/predict_config.js
+++ gp_learner/demo/predict_config.js
@@ -1,3 +1,5 @@
+let MAX_RESULT_ROWS = 100;
 let gpl_endpoint = "http://localhost:8080/api";
+let gpl_endpoint_ping = gpl_endpoint + '/ping';
 let gpl_endpoint_predict = gpl_endpoint + '/predict';
-let gpl_endpoint_feedback = gpl_endpoint + '/feedack';
+let gpl_endpoint_feedback = gpl_endpoint + '/feedback';

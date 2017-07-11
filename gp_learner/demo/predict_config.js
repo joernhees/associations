c51efeeee40af3321a15b403f2ace76e6385e3f1
Joern Hees <dev@joernhees.de>
2017-07-11 21:12:08 +0200

    WIP: get and visualise gps (also tabs)

--- gp_learner/demo/predict_config.js
+++ gp_learner/demo/predict_config.js
@@ -1,5 +1,6 @@
 let MAX_RESULT_ROWS = 100;
 let gpl_endpoint = "http://localhost:8080/api";
 let gpl_endpoint_ping = gpl_endpoint + '/ping';
+let gpl_endpoint_graph_patterns = gpl_endpoint + '/graph_patterns';
 let gpl_endpoint_predict = gpl_endpoint + '/predict';
 let gpl_endpoint_feedback = gpl_endpoint + '/feedback';

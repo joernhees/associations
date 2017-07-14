6eaf65bdfee2735f10c85f9354ca1127940935a2
Joern Hees <dev@joernhees.de>
2017-07-14 03:23:20 +0200

    use https for ajax requests

--- gp_learner/demo/predict_config.js
+++ gp_learner/demo/predict_config.js
@@ -2,7 +2,7 @@ let MAX_RESULT_ROWS = 100;
 let DEFAULT_FUSION_METHOD = 'gp_precisions';
 
 //let gpl_endpoint = "http://localhost:8080/api";
-let gpl_endpoint = "http://projects.dfki.uni-kl.de/graph-pattern-learner/api";
+let gpl_endpoint = "https://projects.dfki.uni-kl.de/graph-pattern-learner/api";
 let gpl_endpoint_ping = gpl_endpoint + '/ping';
 let gpl_endpoint_graph_patterns = gpl_endpoint + '/graph_patterns';
 let gpl_endpoint_predict = gpl_endpoint + '/predict';
